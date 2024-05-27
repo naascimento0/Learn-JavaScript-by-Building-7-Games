541022d3da0489193dbd6f8e28cc0d102dd4199c
naascimento0 <gabrieloliveiran@hotmail.com>
2024-05-27 17:18:32 -0300

    images to 100x100, board created

--- memory_game/app.js
+++ memory_game/app.js
@@ -1,53 +1,65 @@
 const cardArray = [
     {
         name: 'fries',
-        img: 'images/fries.png' 
+        img: 'images/fries' 
     },
     {
         name: 'hamburger',
-        img: 'images/hamburger.png'
+        img: 'images/hamburger'
     },
     {
         name: 'hotdog',
-        img: 'images/hotdog.png'
+        img: 'images/hotdog'
     },
     {
         name: 'ice-cream',
-        img: 'images/ice-cream.png' 
+        img: 'images/ice-cream' 
     },
     {
         name: 'milkshake',
-        img: 'images/milkshake.png' 
+        img: 'images/milkshake'
     },
     {
         name: 'pizza',
-        img: 'images/hambpizzaurger.png'
+        img: 'images/pizza'
     },
     {
         name: 'fries',
-        img: 'images/fries.png' 
+        img: 'images/fries' 
     },
     {
         name: 'hamburger',
-        img: 'images/hamburger.png'
+        img: 'images/hamburger'
     },
     {
         name: 'hotdog',
-        img: 'images/hotdog.png'
+        img: 'images/hotdog'
     },
     {
         name: 'ice-cream',
-        img: 'images/ice-cream.png' 
+        img: 'images/ice-cream' 
     },
     {
         name: 'milkshake',
-        img: 'images/milkshake.png' 
+        img: 'images/milkshake' 
     },
     {
         name: 'pizza',
-        img: 'images/hambpizzaurger.png'
+        img: 'images/pizza'
     }
 ]
 
 cardArray.sort(() => 0.5 - Math.random()) // shuffle the array
-console.log(cardArray)
+
+const gridDisplay = document.querySelector('#grid')
+
+function createBoard(){
+    for(let i = 0; i < cardArray.length; i++){
+        const card = document.createElement('img')
+        card.setAttribute('src', 'images/blank')
+        card.setAttribute('data-id', i)
+        gridDisplay.appendChild(card)
+    }
+}
+
+createBoard()
